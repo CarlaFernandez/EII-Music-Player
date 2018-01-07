71e09f8c640dfff8f4c378903cf6dde5307bfba3
Carla <carla@pitando.com>
2018-01-07 13:56:09 +0100

    Deleted "settings" tab

--- app/src/main/java/com/eii/eiimusicplayer/ui/activities/HomeActivity.java
+++ app/src/main/java/com/eii/eiimusicplayer/ui/activities/HomeActivity.java
@@ -10,8 +10,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.eii.eiimusicplayer.R;
@@ -98,27 +96,6 @@ public class HomeActivity extends AppCompatActivity implements BottomSheetFragme
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
